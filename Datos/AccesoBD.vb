309e4a7bf7f42dbc92314b5052c391b4c4b451d4
Diego Suarez <diegosuarez713@gmail.com>
2017-10-09 14:08:17 -0300

    Metodos abstractos sobre capa de datos, simplificacion de metodos sobre capa de presentacion

---
+++ Datos/AccesoBD.vb
@@ -0,0 +1,30 @@
+﻿Public Class AccesoBD
+    Public Shared Function AbrirConexion()
+        Dim conexion As New Data.SqlClient.SqlConnection
+        Dim cmd As New Data.SqlClient.SqlCommand
+
+        Dim cadenaConexion As String = "Data Source=DESKTOP-PP344HH;Initial Catalog=SistemaGimnasio;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+        conexion.ConnectionString = cadenaConexion
+        conexion.Open()
+        cmd.Connection = conexion
+        cmd.CommandType = CommandType.Text
+
+        Return cmd
+    End Function
+
+    Public Shared Sub CerrarConexion(ByVal conex As Data.SqlClient.SqlConnection)
+        conex.Close()
+    End Sub
+
+    Public Shared Sub ejecutarConsulta(ByVal consulta As String)
+        Dim cmd As New Data.SqlClient.SqlCommand
+        cmd = AbrirConexion()
+
+        cmd.CommandText = consulta
+        cmd.ExecuteNonQuery()
+
+    End Sub
+
+
+
+End Class
